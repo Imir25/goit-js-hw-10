5c833e51fc6bad1cbb8b35eabcd404e7a08ce8e7
Imir25 <anitra26121998@gmail.com>
2023-10-02 00:02:56 +0300

    fixed errors js

--- src/js/index.js
+++ src/js/index.js
@@ -10,40 +10,45 @@ var slim = new SlimSelect({
   select: '#breed-select',
   placeholder: 'Select a breed',
   events: {
-    afterOpen: () => {
+    afterClose: () => {
       catInfo.innerHTML = '';
-      const breedId = slim.getSelected(); 
+      const breedId = slim.getSelected();
       loader.classList.remove('is-hidden');
 
       fetchCatByBreed(breedId)
-        .then((cat) => {
+        .then(cat => {
           renderCat(cat);
           loader.classList.add('is-hidden');
         })
-        .catch((error) => {
-          Notiflix.Notify.failure('Oops! Something went wrong. Try reloading the page.');
+        .catch(error => {
+          Notiflix.Notify.failure(
+            'Oops! Something went wrong. Try reloading the page.'
+          );
           loader.classList.add('is-hidden');
         });
-    }
+    },
   },
 });
 
 window.onload = () => {
   loader.classList.remove('is-hidden');
-
   fetchBreeds()
-    .then((breeds) => {
-      const options = breeds.map((breed) => ({
-        text: breed.name,
-        value: breed.id,
-      }));
+    .then(breeds => {
+      let options = [{ text: '', value: '', placeholder: true }];
+      options.push(
+        ...breeds.map(breed => ({
+          text: breed.name,
+          value: breed.id,
+        }))
+      );
       slim.setData(options);
       breedSelect.classList.remove('is-hidden');
       loader.classList.add('is-hidden');
     })
-    .catch((error) => {
-      Notiflix.Notify.failure('Oops! Something went wrong. Try reloading the page.');
-      breedSelect.classList.remove('is-hidden');
+    .catch(error => {
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong. Try reloading the page.'
+      );
       loader.classList.add('is-hidden');
     });
 };
